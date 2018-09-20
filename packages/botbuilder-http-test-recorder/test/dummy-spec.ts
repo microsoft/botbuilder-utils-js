81613595c98628ab788f4dd7b95b08d6013b9421
Christopher Stone <chstone@microsoft.com>
2018-09-20 16:41:42 -0400

    add code headers (c) (#56)

--- packages/botbuilder-http-test-recorder/test/dummy-spec.ts
+++
@@ -1,9 +0,0 @@
-// import { expect } from 'chai';
-
-// import { add } from '../src';
-
-// describe('Add', () => {
-//   it('should add 2 numbers', () => {
-//     expect(add(1, 2)).to.equal(3);
-//   });
-// });
